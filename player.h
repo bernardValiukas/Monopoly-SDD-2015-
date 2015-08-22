0149c496195f1feed7576628075151a6eaf29899
JackieSan <j.b.valiukas@gmail.com>
2015-08-23 00:15:42 +1000

    Update player.h

--- player.h
+++ player.h
@@ -13,13 +13,30 @@ class Player
 		void setPosition (int playerPosition);											//set position of player
 		int getPosition();																//access player's position
 		void setJailCards(int playerJailCards);											//set number of jail cards for player
-		int getJailCards;																//access number of jail cards for player
+		int getJailCards();																//access number of jail cards for player
 		void setDoublesRolled(int turnDoublesRolled);									//sets number of doubles rolled for player
 		int getDoublesRolled();															//access doubles rolled
 		void setAuctionFlag(bool playerAuctionFlag);
 		bool getAuctionFlag();
 		void setBankruptFlag(bool playerBankruptFlag);
 		bool getBankruptFlag();
+		void setJailTrack(int playerJailTrack);
+		int getJailTrack();
+		void setStationsOwned(int playerStationsOwned);
+		int getStationsOwned();
+		void setUtilitiesOwned(int playerUtilitiesOwned);
+		int getUtilitiesOwned();
+		void setPropertyWorth(int playerPropertyWorth);
+		int getPropertyWorth();
+		void setChanceJailCard(bool playerChanceJailCard);
+		bool getChanceJailCard();
+		void setCommunityJailCard(bool playerCommunityJailCard);
+		bool getCommunityJailCard();
+		void setCancelRent(bool playerCancelRent);
+		bool getCancelRent();
+		void setCancelBuy(bool playerCancelBuy);
+		bool getCancelBuy();
+
 		
 	private:																			//attributes of player instances (MUST use player functions to view or change values)
 		string name;
@@ -30,4 +47,13 @@ class Player
 		int doublesRolled;
 		bool auctionFlag;
 		bool bankruptFlag;
+		int jailTrack;
+		int propertyWorth;
+		int stationsOwned;
+		int utilitiesOwned;
+		bool chanceJailCard;
+		bool communityJailCard;
+		bool cancelRent;
+		bool cancelBuy;
+		
 };
